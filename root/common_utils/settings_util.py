689fcc67717a6ce7eac8a66e28305830ff0a3efa
chris <lilunlogic@163.com>
2020-04-30 16:54:13 +0800

    update settings.xml

--- root/common_utils/settings_util.py
+++ root/common_utils/settings_util.py
@@ -8,7 +8,8 @@ class SettingsUtil:
     __instance = None
 
     __const = Const()
-    __const.MYSQL_SECTION = "mysql"
+    __const.MYSQL_SECTION = "mysql_test"
+    # __const.MYSQL_SECTION = "mysql_prod"
     __const.EMAIL_SECTION = "email"
 
     def __init__(self):
